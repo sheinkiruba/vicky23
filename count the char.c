0f065f9f712c722f9945a50cfc8cecfced5028a7
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-02-12 16:35:24 +0530

    count the character without space

---
+++ count the char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char str[100];
+int len, space;
+printf( "Enter string:\n ");
+printf("\n");
+gets(str);
+len = strlen(str);
+printf("\n");
+printf("\n No.of characters in string is %d " , len );
+}
