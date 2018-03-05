c876f023d26159a4fa34e3557a49afe9cb555326
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-03-05 15:58:44 +0530

    print the string k times

---
+++ k times.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+     char stringArray[100];
+     int k;
+    printf("Please write something: \n");
+    scanf("%s", stringArray);
+    printf("enter the no k: ");
+    scanf("%d",&k);
+    return 0; 
+}
