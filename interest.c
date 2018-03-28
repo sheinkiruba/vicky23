2b8b883b1f2418cc8ae5f9f030e7f4d4f253f4c0
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-03-28 16:54:17 +0530

    calculate simple intersest

---
+++ interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int p,n,r;
+float result;
+scanf("%d",&p);
+scanf("%d %d",&n,&r);
+result=(p*n*r)/100;
+printf("\nthe simple interest amount is %f ",result);
+return 0;
+}
